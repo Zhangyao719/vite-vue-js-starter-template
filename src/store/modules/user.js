53826231724239cda42ff9c44a5ae349994ad367
HK-XXB-张尧 <HK-XXB-@tencent.com>
2025-01-09 08:57:21 +0800

    fix: 解决签到人数不足的问题

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -10,9 +10,24 @@ export const useUserStore = defineStore('user', {
   getters: {},
   actions: {
     getRandomUsers(count = 30) {
-      const startIndex = Math.floor(Math.random() * (this.allSignInUser.length - count + 2));
-      const endIndex = startIndex + count;
-      return this.allSignInUser.slice(startIndex, endIndex);
+      if (this.allSignInUser.length < count) {
+        const cloneList = this.allSignInUser.slice(0);
+        let addNum = count - cloneList.length;
+        // 先从原数组中随机补充缺少的元素
+        while (addNum--) {
+          cloneList.push(this.allSignInUser[Math.floor(Math.random() * this.allSignInUser.length)]);
+        }
+        // 再将 cloneList 打乱
+        for (let i = cloneList.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [cloneList[i], cloneList[j]] = [cloneList[j], cloneList[i]];
+        }
+        return cloneList;
+      } else {
+        const startIndex = Math.floor(Math.random() * (this.allSignInUser.length - count + 2));
+        const endIndex = startIndex + count;
+        return this.allSignInUser.slice(startIndex, endIndex);
+      }
     },
     async fetchAllSignInUser() {
       const data = await getAllSignInUser();
